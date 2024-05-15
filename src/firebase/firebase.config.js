58011ab47481b9c5517fe5a1a9237f54f112fb84
Rakib <rakibulhasan3929@gmail.com>
2024-05-15 17:46:08 +0600

    complete a website

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAHp4syKCINnpCDYKhDcVkbJ7VtSJiX8JU",
-  authDomain: "hotel-booking-client-1d012.firebaseapp.com",
-  projectId: "hotel-booking-client-1d012",
-  storageBucket: "hotel-booking-client-1d012.appspot.com",
-  messagingSenderId: "368510571939",
-  appId: "1:368510571939:web:ecb3fda533c2fa7841f616"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
